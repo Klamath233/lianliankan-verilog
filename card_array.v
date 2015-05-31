334922e249d6bc971582c7035f5c17423f4740b0
Xi Han <xihan94@ucla.edu>
2015-05-30 18:08:06 -0700

    Add test to card array module and test it.

--- card_array.v
+++ card_array.v
@@ -24,49 +24,22 @@ module card_array(clk, rst, s, mf, ms, cur_bus,
   output [35:0] blink_bus; // the blink bus output.
   output [35:0] hidden_bus; // the hidden bus output.
 
-  // The internal buses which divide the signals.
-  wire [35:0] __clk_bus;
-  wire [35:0] __rst_bus;
-  wire [35:0] __s_bus;
-  wire [35:0] __mf_bus;
-  wire [35:0] __ms_bus;
-
-  // The driver driving the bus.
-  reg [35:0] __clk_bus_driver;
-  reg [35:0] __rst_bus_driver;
-  reg [35:0] __s_bus_driver;
-  reg [35:0] __mf_bus_driver;
-  reg [35:0] __ms_bus_driver;
-  
   generate
     genvar i;
     for (i = 0; i < 36; i = i + 1) begin
-      always @* begin
-        __clk_bus_driver[i] = clk;
-        __rst_bus_driver[i] = rst;
-        __s_bus_driver[i] = s;
-        __mf_bus_driver[i] = mf;
-        __ms_bus_driver[i] = ms;
-      end
+      card card(
+        .clk(clk),
+        .rst(rst),
+        .cur(cur_bus[i]),
+        .s(s),
+        .mf(mf),
+        .ms(ms),
+        .sel(sel_bus[i]),
+        .blink(blink_bus[i]),
+        .hidden(hidden_bus[i]),
+        .state_debug()
+      );
     end
   endgenerate
-
-  assign __clk_bus = __clk_bus_driver;
-  assign __rst_bus = __rst_bus_driver;
-  assign __s_bus = __s_bus_driver;
-  assign __mf_bus = __mf_bus_driver;
-  assign __ms_bus = __ms_bus_driver;
   
-  card card_array[35:0](
-    .clk(__clk_bus),
-    .rst(__rst_bus),
-    .cur(__cur_bus),
-    .s(__s_bus),
-    .mf(__mf_bus),
-    .ms(__ms_bus),
-    .sel(__sel_bus),
-    .blink(__blink_bus),
-    .hidden(__hidden_bus)
-  );
-
 endmodule
---
+++ card_array_test.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+
+module card_array_test;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg s;
+	reg mf;
+	reg ms;
+	reg [35:0] cur_bus;
+
+	// Outputs
+	wire [35:0] sel_bus;
+	wire [35:0] blink_bus;
+	wire [35:0] hidden_bus;
+
+	// Instantiate the Unit Under Test (UUT)
+	card_array uut (
+		.clk(clk), 
+		.rst(rst), 
+		.s(s), 
+		.mf(mf), 
+		.ms(ms), 
+		.cur_bus(cur_bus), 
+		.sel_bus(sel_bus), 
+		.blink_bus(blink_bus), 
+		.hidden_bus(hidden_bus)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		s = 0;
+		mf = 0;
+		ms = 0;
+		cur_bus = 36'b100000000000000000000000000000000000;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+    cur_bus = 36'b010000000000000000000000000000000000;
+
+    #100;
+    cur_bus = 36'b001000000000000000000000000000000000;    
+		// Add stimulus here
+
+	end
+  always #5 clk = ~clk;
+endmodule
+
